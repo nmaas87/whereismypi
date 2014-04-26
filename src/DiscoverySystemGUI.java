0a11c1f0a6686eca24707a5f570b1ee4fc3a4d93
Nico Maas <xenomorph150@gmail.com>
2014-04-26 15:23:41 +0200

    - Project Logo - Some Renaming in the GUI System

--- src/DiscoverySystemGUI.java
+++ src/DiscoverySystemGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.Image;
 
 public class DiscoverySystemGUI extends Thread {
 // Swing
@@ -6,17 +7,20 @@ protected static DefaultListModel listenModell = new DefaultListModel();
  
 public void run(){ 
 JFrame dS = new JFrame();
+ImageIcon icon = new ImageIcon("wmp.png");
+Image image = icon.getImage();
 JPanel panel = new JPanel();
-JList endListe = new JList(listenModell);
-endListe.setVisibleRowCount(15);
-endListe.setFixedCellHeight(20);
-endListe.setFixedCellWidth(245);
-JScrollPane endListe2 = new JScrollPane(endListe);
+JList zwischenListe = new JList(listenModell);
+zwischenListe.setVisibleRowCount(15);
+zwischenListe.setFixedCellHeight(20);
+zwischenListe.setFixedCellWidth(245);
+JScrollPane fertigeListe = new JScrollPane(zwischenListe);
 
       dS.setTitle("Where is my Pi?");
       dS.setSize(260, 340);
-      dS.setResizable(false);;
-      panel.add(endListe2);
+      dS.setIconImage(image);
+      dS.setResizable(false);
+      panel.add(fertigeListe);
       dS.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
       dS.add(panel);
       dS.setVisible(true); 
